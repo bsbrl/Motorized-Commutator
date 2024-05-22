5d69afa4908951c28b1c1da785a35c90d69255ee
IbrahimOladepo <ibrahim.oladepo@yahoo.com>
2024-05-22 14:34:59 -0500

    Latex customization

--- Software/GUI/Streamlit_Testing.py
+++ Software/GUI/Streamlit_Testing.py
@@ -8,23 +8,30 @@ st.markdown("# Motorized Commutator")
 
 st.write("")
 
-container = st.container(border=True)
+dlc_container = st.container(border=True)
 
-container.write("### DLC Config")
+dlc_container.write("### DLC Config")
 
-col1, col2 = container.columns([2, 1])
-
-address = col1.text_input(
-    "Select DLC Model",
+dlc_Select = dlc_container.text_input(
+    r"$\textsf{\normalsize Select DLC Model}$",
     key="address",
 )
 
-radius = col2.slider(
-    "Display Tracking",
-    100,
-    1500,
-    key="radius",
-)
+dlc_container.checkbox(r"$\textsf{\normalsize Display Tracking}$", key="display")
+
+# col1, col2 = container.columns([2, 1])
+
+# address = col1.text_input(
+#     "Select DLC Model",
+#     key="address",
+# )
+
+# radius = col2.slider(
+#     "Display Tracking",
+#     100,
+#     1500,
+#     key="radius",
+# )
 
 # style: str = col3.selectbox(
 #     "Color theme",
